6d954d4b16af5c12110715f34d99f80ab31d9d54
Kennethcp <kenneth.chr2003@mail.ugm.ac.id>
2024-11-29 15:42:40 +0700

    contact us

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -12,14 +12,13 @@ const ContactUs = () => {
       <div className="flex flex-col lg:flex-row items-center gap-28 p-4 mt-32 mx-auto">
         {/* Map Section */}
         <div className="w-full lg:w-1/2 h-96 bg-white shadow-md rounded-lg">
-        <iframe
-        title="Location Map"
-        className="w-full h-full rounded-lg"
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3966.3310427589913!2d110.38025580767108!3d-7.74163139111216!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwNDQnMzAuMSJTIDExMMKwMjInNDguOSJF!5e0!3m2!1sen!2sid!4v1633788597741"
-        allowFullScreen=""
-        loading="lazy"
-        ></iframe>
-
+          <iframe
+            title="Location Map"
+            className="w-full h-full rounded-lg"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3966.3310427589913!2d110.38025580767108!3d-7.74163139111216!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwNDQnMzAuMSJTIDExMMKwMjInNDguOSJF!5e0!3m2!1sen!2sid!4v1633788597741"
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>
         </div>
 
         {/* Contact Form Section */}
@@ -47,33 +46,31 @@ const ContactUs = () => {
         </div>
       </div>
 
-      {/* Submit Button */}
-      <div className="flex justify-end mt-2 mr-56">
+      {/* Submit and Social Media Buttons */}
+      <div className="flex flex-col lg:flex-row items-center justify-between px-32 gap-6">
+        <div className="flex flex-col gap-2">
+          <a
+            href="#"
+            className="flex items-center bg-ijo shadow-lg text-white px-6 py-2 w-64 h-12 rounded-lg hover:bg-green-600"
+          >
+            <span className="mr-2">
+              <img className ="w-12 -ml-1" src={whatsapp} alt="WhatsApp Icon" />
+            </span>
+            Chat On WhatsApp
+          </a>
+          <a
+            href="#"
+            className="flex items-center bg-purple shadow-lg text-white px-6 py-2 w-64 h-12 rounded-lg hover:bg-purple-600"
+          >
+            <span className="mr-2">
+              <img className ="w-8" src={instagram} alt="Instagram Icon" />
+            </span>
+            Follow On Instagram
+          </a>
+        </div>
         <button className="bg-deepblue text-white font-bold py-3 px-12 rounded-lg shadow-lg hover:bg-blue-700">
           Send
         </button>
-      </div>
-
-      {/* Social Media Buttons */}
-      <div className="flex flex-col lg:flex-row gap-4 mt-6 ml-48">
-        <a
-          href="#"
-          className="flex items-center bg-ijo shadow-lg text-white px-6 rounded-lg hover:bg-green-600"
-        >
-          <span className="mr-2">
-            <img src={whatsapp} alt="WhatsApp Icon" />
-          </span>
-          Chat On WhatsApp
-        </a>
-        <a
-          href="#"
-          className="flex items-center bg-purple shadow-lg text-white py-2 px-6 rounded-lg hover:bg-purple-600"
-        >
-          <span className="mr-2">
-            <img src={instagram} alt="Instagram Icon" />
-          </span>
-          Follow On Instagram
-        </a>
       </div>
     </div>
   );
